b4ae0b1a374dcdb11847ef0662cf1f1e9d07d7b0
EddieH <ehajek@outlook.com>
2021-08-24 13:30:46 -0400

    Created Routes to Note.html

---
+++ routes/apiRoutes/notes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+
+  router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
+  router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    if (!validateNotes(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+      } else {
+        const notes = createNewNotes(req.body, notes);
+        res.json(notes);
+      }
+    });
+    
+
+  module.exports = router;
