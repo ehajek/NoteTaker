fe65070a6fb35148709bd84a5ff66cfb33f04913
EddieH <ehajek@outlook.com>
2021-08-25 16:34:51 -0400

    final Code (clean up indents, extra from building, and updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # NoteTaker
+
+This is a workup of the NoteTaker Site. 
+The project plan is to follow the following criteria; 
+
+GIVEN a note-taking application
+WHEN I open the Note Taker
+THEN I am presented with a landing page with a link to a notes page
+WHEN I click on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page
+WHEN I click on the Save icon
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
+WHEN I click on the Write icon in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+
+## Mockup Image
+
+The website mockup image loaction is "./11-express-homework-demo-01.png" 
+
+
+## Sample Site
+
+The sample site for the NoteTaker Homepage is lockated at "https://ehajek.github.io/NoteTaker/". 
+
+## GitHub Repository 
+
+The name of the NoteTaker GitHub repository is "NoteTaker" at the URL of "https://github.com/ehajek/NoteTaker.git". 
+
+--- 
+
+Eddie Hajek 
+ehajek@outlook.com 
+2021.08.25
--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,60 +1,50 @@
 const router = require('express').Router();
-//const uniqid = require('uniqid'); 
-const { filterByQuery, findById, createNewNotes, validateNotes, deleteNotes } = require('../../lib/notes');
+const { findById, createNewNotes, validateNotes, deleteNotes } = require('../../lib/notes');
 
 const notes = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     let results = notes;
-    //console.log(results);
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-      //console.log(results);
-    }
     res.json(results);
-  });
+});
 
-
-  router.get('/notes/:id', (req, res) => {
+router.get('/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
     if (result) {
-      res.json(result);
-      console.log(res.json(result));
+        res.json(result);
+        console.log(res.json(result));
     } else {
-      res.send(404);
+        res.send(404);
     }
-  });
+});
 
-  router.post('/notes', (req, res) => {
-    // set id based on what the next index of the array will be
-    //console.log(notes);
-    req.body.id = notes.length.toString();
-  
-    if (!validateNotes(req.body)) {
-        res.status(400).send('The note is not properly formatted.');
-      } else {
-        //   console.log('Test NewNote');
-        //   console.log(req.body)
-        const note = createNewNotes(req.body, notes);
+router.post('/notes', (req, res) => {
+//console.log(notes);
+req.body.id = notes.length.toString();
 
-        res.json(note);
-      }
-    });
+if (!validateNotes(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+} else {
+    //   console.log('Test NewNote');
+    //   console.log(req.body)
+    const note = createNewNotes(req.body, notes);
+    res.json(note);
+    }
+});
 
-    router.delete('/notes/:id', (req, res) => {
-        const result = findById(req.params.id, notes);
-        console.log("Testing Before");
+router.delete('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    console.log("Testing Before");
+    console.log(notes);
+    if (result) {
+        notes.splice(result.id, 1)
+        console.log("Testing After");
         console.log(notes);
-        if (result) {
-            notes.splice(result.id, 1)
-            console.log("Testing After");
-            console.log(notes);
-            deleteNotes(notes);
-            res.json(notes);
-        } else {
-            res.send(404);
-        }
-      });
+        deleteNotes(notes);
+        res.json(notes);
+    } else {
+        res.send(404);
+    }
+});
     
-
-  module.exports = router;
+module.exports = router;
