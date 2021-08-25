ac80e6d96088c155a353b5ab0299d60e2eb36ede
EddieH <ehajek@outlook.com>
2021-08-25 16:14:50 -0400

    Added Delete function and Delete Post

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 //const uniqid = require('uniqid'); 
-const { filterByQuery, findById, createNewNotes, validateNotes } = require('../../lib/notes');
+const { filterByQuery, findById, createNewNotes, validateNotes, deleteNotes } = require('../../lib/notes');
 
 const notes = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     let results = notes;
-    console.log(results);
+    //console.log(results);
     if (req.query) {
       results = filterByQuery(req.query, results);
       //console.log(results);
@@ -19,6 +19,7 @@ router.get('/notes', (req, res) => {
     const result = findById(req.params.id, notes);
     if (result) {
       res.json(result);
+      console.log(res.json(result));
     } else {
       res.send(404);
     }
@@ -26,7 +27,7 @@ router.get('/notes', (req, res) => {
 
   router.post('/notes', (req, res) => {
     // set id based on what the next index of the array will be
-    console.log(notes);
+    //console.log(notes);
     req.body.id = notes.length.toString();
   
     if (!validateNotes(req.body)) {
@@ -39,6 +40,21 @@ router.get('/notes', (req, res) => {
         res.json(note);
       }
     });
+
+    router.delete('/notes/:id', (req, res) => {
+        const result = findById(req.params.id, notes);
+        console.log("Testing Before");
+        console.log(notes);
+        if (result) {
+            notes.splice(result.id, 1)
+            console.log("Testing After");
+            console.log(notes);
+            deleteNotes(notes);
+            res.json(notes);
+        } else {
+            res.send(404);
+        }
+      });
     
 
   module.exports = router;
